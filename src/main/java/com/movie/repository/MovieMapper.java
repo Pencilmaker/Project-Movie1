1118e01e71e14f7765229bb20c3f935ef36106c4
Deon <Deon@Deon>
2023-04-26 10:03:19 +0900

    2023년 4월 26일 변경

--- src/main/java/com/movie/repository/MovieMapper.java
+++ src/main/java/com/movie/repository/MovieMapper.java
@@ -7,7 +7,10 @@ import com.movie.model.Movie;
 
 @Mapper
 public interface MovieMapper {
+	
+	// 클릭 시 저장
 	int saveId(Movie movie);
 	
+	// 클릭 시 중복 ID가 있는지 확인 용도
 	Movie findId(String id);
 }
--- src/main/resources/static/css/asd/script.js
+++ src/main/resources/static/css/asd/script.js
@@ -1,6 +1,6 @@
 const apiKey = 'dfcdb12192ff1bc68ebd64bb5f86f67e';
 
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ko-KR&page=1`)
+fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ko-KR&page=2`)
   .then(response => response.json())
   .then(data => {
     const movies = data.results;
@@ -12,7 +12,7 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ko-
       movieElement.classList.add('movie');
 
       const imgLinkElement = document.createElement('a');
-      imgLinkElement.href = `https://www.themoviedb.org/movie/${movie.id}`;
+      imgLinkElement.href = `https://localhost:9000/movie/${movie.id}`;
       imgLinkElement.target = '_blank';
 
       const imgElement = document.createElement('img');
@@ -33,9 +33,9 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ko-
       idElement.textContent = `id: ${movie.id}`;
       movieElement.appendChild(idElement);
 
-      const overviewElement = document.createElement('p');
+/*      const overviewElement = document.createElement('p');
       overviewElement.textContent = `Overview: ${movie.overview}`;
-      movieElement.appendChild(overviewElement);
+      movieElement.appendChild(overviewElement);*/
 
       imgLinkElement.addEventListener('click', () => {
         const xhr = new XMLHttpRequest();
@@ -43,7 +43,7 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ko-
         xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
         xhr.send(JSON.stringify({ id: movie.id, title: movie.title, overview: movie.overview })); // id 데이터 전송
 
-        alert(`Movie ID: ${movie.id}`);
+        alert(`Movie ID: ${movie.id}`); // 이미지 클릭 시 알림창
       });
 
       moviesContainer.appendChild(movieElement);
